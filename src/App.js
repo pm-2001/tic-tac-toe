ab98ac59b130bae52466900322a9fae724066158
pm-2001 <princemauryapm2001@gmail.com>
2023-06-27 20:11:39 +0530

    game is functional

--- src/App.js
+++ src/App.js
@@ -11,15 +11,35 @@ function Square({value,onSquareClick}){
 }
 
 export default function Board() {
+  const [isXNext,setIsXNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
+    if(squares[i] || calculateWinner(squares)){ return;}
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if(isXNext){
+      nextSquares[i] = 'X';
+    }
+    else{
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setIsXNext(!isXNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = 'Winner: '+winner;
+    alert('Congratulations');
+  }
+  else{
+    status = 'Next player: '+(isXNext?'X':'O');
+  }
+
   return (
   <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value = {squares[0]} onSquareClick = {()=>handleClick(0)}/>
       <Square value = {squares[1]} onSquareClick = {()=>handleClick(1)}/>
@@ -39,4 +59,26 @@ export default function Board() {
 
   </>
   )
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i = 0; i<lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
